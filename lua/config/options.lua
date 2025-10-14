329b7d1332056ef8453bf8d8645cb1a04cec7db0
ZackShem <167593632+zohvik@users.noreply.github.com>
2025-10-14 18:00:40 -0400

    Add files via upload

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+
+
+
+
+
+-- s
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		"lua",
+		"cpp",
+	    },
+	    auto_install = false,
+	})
+
+    end
+
+}
+
+
